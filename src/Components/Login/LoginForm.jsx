788e02fe0f49aa23803795d6a35fa0369f44a29b
ManoelDev-ifma <lucas.reis.ufma@gmail.com>
2026-06-18 10:14:56 -0300

    feat: add API request config helpers

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,39 +2,50 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { getTokenRequest, getUserRequest } from "../../api";
+import { useEffect } from "react";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  // Precisa ser transformada em uma async function.
-  function handleSubmit(event) {
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = getUserRequest(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    if(!username.validate() && !password.validate()) return;
-
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
+    // Não realiza a requisição os inputs não estejam válidos.
+    if (username.validate() && password.validate()) {
+      const { url, options } = getTokenRequest({
+        username: username.value,
+        password: password.value,
       });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" {...username}/>
-        <Input label="Senha" type="text" name="password" {...password}/>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="text" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Criar Conta</Link>
