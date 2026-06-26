2c2cb60c82312b3911c8e82dcf716d352aa6630a
ManoelDev-ifma <lucas.reis.ufma@gmail.com>
2026-06-25 21:27:38 -0300

    feat: add scroll infinite  to feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,16 +1,52 @@
-import FeedModal from './FeedModal';
-import FeedPhotos from './FeedPhotos';
-import { useState } from 'react';
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
+import { useState, useEffect } from "react";
 
-const Feed = () => {
+const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = useState(null);
+  const [pages, setPages] = useState([1]);
+  const [infinite, setInfinite] = useState(true);
+
+  useEffect(() => {
+    function infiniteScroll() {
+      if (!infinite) return;
+      let wait = false;
+      const scroll = window.scrollY;
+      const height = document.body.offsetHeight - window.innerHeight;
+      if (scroll > height * 0.75 && !wait) {
+        setPages((pages) => [...pages, pages.length + 1]);
+        wait = true;
+        setTimeout(() => {
+          wait = false;
+        }, 500);
+      }
+    }
+    infiniteScroll();
+
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
+    return () => {
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
+    };
+  }, [user, infinite]);
 
   return (
     <div>
-      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto}/>}
-      <FeedPhotos setModalPhoto={setModalPhoto}/>
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      {pages.map((page) => (
+        <FeedPhotos
+          key={page}
+          user={user}
+          page={page}
+          setModalPhoto={setModalPhoto}
+          setInfinite={setInfinite}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Feed;
--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,32 +1,41 @@
-import FeedPhotosItem from './FeedPhotosItem';
-import useFetch from '../../Hooks/useFetch';
-import { useEffect } from 'react';
-import { getPhotos } from '../../api';
-import Error from '../Errors/Error';
-import Loading from '../Helpers/Loading';
-import styles from './FeedPhotos.module.css';
+import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
+import { useEffect } from "react";
+import { getPhotos } from "../../api";
+import Error from "../Errors/Error";
+import Loading from "../Helpers/Loading";
+import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = ({setModalPhoto}) => {
-
+const FeedPhotos = ({ user, page, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = getPhotos({ page: 1, total: 8, user: 0 });
-      await request(url, options);
+      const total = 3;
+      const { url, options } = getPhotos({ page, total, user });
+      const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) {
+        setInfinite(false);
+      }
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
-  if (error) return <Error error={error} />
-  if (loading) return <Loading />
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
   if (data)
     return (
       <ul className={`animeLeft ${styles.feed}`}>
-          {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto}/>)}
+        {data.map((photo) => (
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
+        ))}
       </ul>
-  );
+    );
   else return null;
-}
+};
 
 export default FeedPhotos;
